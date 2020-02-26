67337b1bb2407695730f3a474f9fb2a348f47bc4
Tom Pound <tp830@its.york.ac.uk>
2020-02-26 16:42:22 +0000

    XML parses nodes

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -12,6 +12,8 @@ import javax.xml.validation.Validator;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -21,9 +23,70 @@ public class XMLParser {
 	static String xmlName = "src/xml.xml";
 	static Document xmlDoc;
 
-	
 	public static void XMLParser(String fileDir) {
 		xmlDoc = getDocument(fileDir);
+		xmlDoc.getDocumentElement().normalize();
+		
+		System.out.println("Root element: " + xmlDoc.getDocumentElement().getNodeName());
+
+		NodeList slideList = xmlDoc.getElementsByTagName("slide");
+		System.out.println("Number of slides: " + slideList.getLength());
+		
+		
+		for(int i=0; i<slideList.getLength();i++) {
+			Node slideNode = slideList.item(i);
+			System.out.println("Current Element: " + slideNode.getNodeName());
+			if(slideNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element slideElement = (Element) slideNode;
+				System.out.println("Slide id: " + slideElement.getElementsByTagName("id").item(0).getTextContent());
+				System.out.println("Slide duration: " + slideElement.getElementsByTagName("duration").item(0).getTextContent());
+				
+				//System.out.println("Slide text: " + slideElement.getElementsByTagName("text").item(0).getTextContent());
+				System.out.println(slideElement.getFirstChild().getNodeName());
+				
+				
+				/*while(slideElement.getNextSibling() != null) {
+					try{
+						slideElement = (Element) slideElement.getNextSibling();
+						System.out.println("\t "+ slideElement.getNextSibling().getNodeName() + slideElement.getNextSibling().getTextContent());
+					}
+					catch(Exception e) {
+						break;
+					}
+				}*/
+				
+				
+				
+				/*System.out.println("Slide line: " + slideElement.getElementsByTagName("line").item(0).getTextContent());
+				System.out.println("Slide id: " + slideElement.getElementsByTagName("id").item(0).getTextContent());
+				
+				System.out.println("Slide shape: " + slideElement.getElementsByTagName("shape").item(0).getTextContent());
+				System.out.println("Slide id: " + slideElement.getElementsByTagName("id").item(0).getTextContent());
+				
+				System.out.println("Slide audio: " + slideElement.getElementsByTagName("audio").item(0).getTextContent());
+				System.out.println("Slide id: " + slideElement.getElementsByTagName("id").item(0).getTextContent());
+				
+				System.out.println("Slide image: " + slideElement.getElementsByTagName("image").item(0).getTextContent());
+				System.out.println("Slide id: " + slideElement.getElementsByTagName("id").item(0).getTextContent());
+				
+				System.out.println("Slide video: " + slideElement.getElementsByTagName("video").item(0).getTextContent());
+				System.out.println("Slide id: " + slideElement.getElementsByTagName("id").item(0).getTextContent());*/
+
+				
+				
+				
+				
+				
+				
+				
+			}
+			
+			
+			
+		}
+		
+		
+		
 		Element root = xmlDoc.getDocumentElement();
 		Element slide = (Element)root.getFirstChild();
 		Slide s;
@@ -31,7 +94,6 @@ public class XMLParser {
 			s = getSlide(slide);
 			System.out.println(s.title + "," + s.slideNo);
 		}
-		
 	}
 	
 	public static Document getDocument(String name) {
@@ -49,13 +111,32 @@ public class XMLParser {
 		return null;
 	}
 	
+	public static void main(String[] args) {
+		//Schema schema = loadSchema(schemaName);
+		//xmlDoc = getDocument(xmlName);
+		//validateXML(xmlDoc, schema);
+		XMLParser(xmlName);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	private static Slide getSlide(Element e) {
 		String slideTitle = e.getAttribute("title");
 		String slideNoString = e.getAttribute("slideNo");
 		int slideNo = Integer.parseInt(slideNoString);
-		
 		return new Slide(slideTitle, slideNo);
-		
 	}
 	
 	private static class Slide{
@@ -90,11 +171,5 @@ public class XMLParser {
 		return schema;
 	}
 	
-	public static void main(String[] args) {
-		//Schema schema = loadSchema(schemaName);
-		xmlDoc = getDocument(xmlName);
-		//validateXML(xmlDoc, schema);
-		
-		XMLParser(xmlName);
-	}
+
 }
